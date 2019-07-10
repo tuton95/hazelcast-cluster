d620c3e83924a2a5c8bfa0d33d0ef38453842070
tuton <tu.ton@tiki.vn>
2019-07-10 16:10:38 +0700

    change hazelcast config xml

--- src/main/java/vn/tiki/hazelcast_cluster/Main.java
+++ src/main/java/vn/tiki/hazelcast_cluster/Main.java
@@ -5,10 +5,38 @@ import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
 
 public class Main {
     public static void main(String[] args) {
-        Config config = new XmlConfigBuilder().build();
-        HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
+        var main = new Main();
+        main.run();
+    }
+
+    public void run() {
+        try {
+            var env = System.getenv("env_name");
+            System.out.println("ENV_NAME: " + env);
+            XmlConfigBuilder builder = null;
+            if (env == null || env.equals("local")) {
+                builder = new XmlConfigBuilder(resourcesPath("/thanos-hazelcast-local.xml"));
+            } else if (env.equals("uat")) {
+                builder = new XmlConfigBuilder(resourcesPath("/thanos-hazelcast-uat.xml"));
+            } else {
+                builder = new XmlConfigBuilder(resourcesPath("/thanos-hazelcast-production.xml"));
+            }
+            Config config = builder.build();
+            HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
+        } catch (FileNotFoundException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String resourcesPath(String filename) throws URISyntaxException {
+        var file = new File(this.getClass().getResource(filename).toURI());
+        return file.getPath();
     }
 }
